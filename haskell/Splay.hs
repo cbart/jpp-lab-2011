ba09489c9be3476567bd0984cd2efea777b9739d
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2011-02-22 16:33:39 +0100

    Proper names in Splay

--- haskell/Splay.hs
+++ haskell/Splay.hs
@@ -1,33 +1,35 @@
 module Splay
-( empty1
-, insert1
-, lookup1
-, fromList1
-, toList1
+( empty
+, insert
+, lookup
+, fromList
+, toList
 ) where
+
+import Prelude hiding(lookup)
 
 data (Ord k) => Dict k v = Node (k, v) (Dict k v) (Dict k v) | Empty deriving (Show)
 
-empty1 :: Dict k v
-empty1 = Empty
+empty :: Dict k v
+empty = Empty
 
-insert1 :: (Ord k) => k -> v -> Dict k v -> Dict k v
-insert1 k v Empty = Node (k, v) Empty Empty
-insert1 k v (Node p@(k', _) l r)
-    | k == k' = Node (k, v) l r
-    | k < k' = Node p (insert1 k v l) r
-    | otherwise = Node p l (insert1 k v r)
+insert :: (Ord k) => k -> v -> Dict k v -> Dict k v
+insert k v Empty = Node (k, v) Empty Empty
+insert k v (Node p@(k', _) l r)
+    | k < k' = Node p (insert k v l) r
+    | k > k' = Node p l (insert k v r)
+    | otherwise = Node (k, v) l r
 
-lookup1 :: (Ord k) => k -> Dict k v -> Maybe v
-lookup1 k Empty = Nothing
-lookup1 k (Node (k', v) l r)
-    | k == k' = Just v
-    | k < k' = lookup1 k l
-    | otherwise = lookup1 k r
+lookup :: (Ord k) => k -> Dict k v -> Maybe v
+lookup k Empty = Nothing
+lookup k (Node (k', v) l r)
+    | k < k' = lookup k l
+    | k > k' = lookup k r
+    | otherwise = Just v
 
-fromList1 :: (Ord k) => [(k, v)] -> Dict k v
-fromList1 = foldl (flip $ uncurry insert1) Empty
+fromList :: (Ord k) => [(k, v)] -> Dict k v
+fromList = foldl (flip $ uncurry insert) Empty
 
-toList1 :: (Ord k) => Dict k v -> [(k, v)]
-toList1 Empty = []
-toList1 (Node p l r) = (toList1 l) ++ [p] ++ (toList1 r)
+toList :: (Ord k) => Dict k v -> [(k, v)]
+toList Empty = []
+toList (Node p l r) = (toList l) ++ [p] ++ (toList r)
