4dbff847ce439e3fd01527ba20717ca3eef0beba
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2011-02-22 19:34:05 +0100

    First task (with stdin/out instead of fd)

---
+++ haskell/Main.hs
@@ -0,0 +1,40 @@
+module Main where
+
+import Splay
+import System.IO
+import Data.List(sortBy)
+
+hGetLine' :: Handle -> IO (Maybe String)
+hGetLine' input = do
+    eof <- hIsEOF input
+    if eof
+        then return Nothing
+        else hGetLine input >>= return . Just
+
+parseLines :: (Integral n) => Handle -> Dict String n -> IO (Dict String n)
+parseLines input d =
+    do
+        mline <- hGetLine' input
+        case mline of
+            Nothing -> return d
+            Just line -> parseLines input $ foldr countWord d $ filter ((> 2) . length) $ words line
+    where
+        countWord :: (Integral n) => String -> Dict String n -> Dict String n
+        countWord w d = insert w count d
+            where count = maybe 1 (+1) $ Splay.lookup w d
+
+printLines :: (Integral n) => Handle -> [(String, n)] -> IO [(String, n)]
+printLines _ [] = return []
+printLines output (p:ps) = do
+    hPutStrLn output $ toString p
+    printLines output ps
+    where
+        toString :: (Integral n) => (String, n) -> String
+        toString (s, n) = s ++ " " ++ show n
+
+main :: IO ()
+main = do
+    parsed <- parseLines stdin empty
+    let revSwapCompare b a = compare (snd a, fst a) (snd b, fst b)
+    _ <- printLines stdout $ take 10 $ sortBy revSwapCompare $ toList parsed
+    return ()
--- haskell/Splay.hs
+++ haskell/Splay.hs
@@ -1,31 +1,36 @@
 module Splay
-( empty
+( Dict
+, empty
 , insert
 , lookup
 , fromList
 , toList
+, size
 ) where
 
 import Prelude hiding(lookup)
 
-data (Ord k) => Dict k v = Node (k, v) (Dict k v) (Dict k v) | Empty deriving (Show)
+data (Ord k) => Dict k v =
+    Node (k, v) (Dict k v) (Dict k v)
+  | Empty
+    deriving (Show)
 
 empty :: Dict k v
 empty = Empty
 
 insert :: (Ord k) => k -> v -> Dict k v -> Dict k v
 insert k v Empty = Node (k, v) Empty Empty
-insert k v (Node p@(k', _) l r)
-    | k < k' = Node p (insert k v l) r
-    | k > k' = Node p l (insert k v r)
-    | otherwise = Node (k, v) l r
+insert k v (Node p@(k', _) l r) = case compare k k' of
+    LT -> Node p (insert k v l) r
+    GT -> Node p l (insert k v r)
+    EQ -> Node (k, v) l r
 
 lookup :: (Ord k) => k -> Dict k v -> Maybe v
 lookup k Empty = Nothing
-lookup k (Node (k', v) l r)
-    | k < k' = lookup k l
-    | k > k' = lookup k r
-    | otherwise = Just v
+lookup k (Node (k', v) l r) = case compare k k' of
+    LT -> lookup k l
+    GT -> lookup k r
+    EQ -> Just v
 
 fromList :: (Ord k) => [(k, v)] -> Dict k v
 fromList = foldl (flip $ uncurry insert) Empty
@@ -33,3 +38,7 @@ fromList = foldl (flip $ uncurry insert) Empty
 toList :: (Ord k) => Dict k v -> [(k, v)]
 toList Empty = []
 toList (Node p l r) = (toList l) ++ [p] ++ (toList r)
+
+size :: (Ord k) => Dict k v -> Int
+size Empty = 0
+size (Node _ l r) = 1 + size l + size r
