1a097ef51236d93b71c6687bc6a5de28eda3da63
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2011-02-25 12:04:33 +0100

    Splay fixes

--- haskell/dict/Splay.hs
+++ haskell/dict/Splay.hs
@@ -17,6 +17,11 @@ data (Ord k) => Dict k v =
 empty :: Dict k v
 empty = Empty
 
+foldDict :: (Ord k) => k -> Dict k v -> (a -> Dict k v -> a) -> a -> a
+foldDict _ Empty _ a = a
+foldDict k n@(Node (k', _) l r) f a = foldDict k s f (f a n)
+    where s = case compare k k' of { LT -> l; GT -> r; EQ -> Empty }
+
 insert :: (Ord k) => k -> v -> Dict k v -> Dict k v
 insert k v Empty = Node (k, v) Empty Empty
 insert k v (Node p@(k', _) l r) = case compare k k' of
@@ -36,4 +41,8 @@ fromList = foldl (flip $ uncurry insert) Empty
 
 toList :: (Ord k) => Dict k v -> [(k, v)]
 toList Empty = []
-toList (Node p l r) = (toList l) ++ [p] ++ (toList r)
+toList (Node p l r) = (toList l) ++ (p : toList r)
+
+--splay :: (Ord k) => k -> Dict k v -> Dict k v
+--splay k Empty = Empty
+--splay k d = zigzag LT >>
