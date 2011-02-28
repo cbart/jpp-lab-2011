ba2f7262f80b3fc09d5baf0c577730171b93ecfb
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2011-03-01 00:29:07 +0100

    Dict progress

--- haskell/dict/Main.hs
+++ haskell/dict/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
 import Prelude hiding(lookup)
@@ -7,39 +9,45 @@ import Data.Char
 import Data.List(sortBy)
 import System.Environment(getArgs)
 
-parseString :: (Integral n) => String -> Dict String n
-parseString = foldr addWord empty . validWords
-
-addWord :: (Integral n) => String -> Dict String n -> Dict String n
-addWord s dict = insert s c' dict
-    where c' = maybe 1 (+1) $ lookup s dict
-
-validWords :: String -> [String]
-validWords s = filter (longerThan 2) $ words $ map letterOrSpace s
-
-longerThan :: (Integral n) => n -> String -> Bool
-longerThan 0 (_:_) = True
-longerThan n (_:cs) = longerThan (n - 1) cs
-longerThan _ [] = False
-
-letterOrSpace :: Char -> Char
-letterOrSpace c = if isLetter c then c else ' '
-
-printTopWords :: (Integral n) => Dict String n -> IO ()
-printTopWords dict = do
-    putStr $ unlines $ map toString' $ take 10 $ sortBy (flip wordCounts) $ toList dict
-
-toString' :: (Integral n) => (String, n) -> String
-toString' (s, n) = s ++ " " ++ show n
-
-wordCounts :: (Integral n) => (String, n) -> (String, n) -> Ordering
-wordCounts (_, n) (_, m) = compare n m
-
 main :: IO ()
 main = do
     args <- getArgs
-    fileHandle <- openFile (head args) ReadMode
-    fileContent <- hGetContents fileHandle
-    let dict = parseString fileContent
-    printTopWords dict
-    hClose fileHandle
+    file_content <- readFile $ head args
+    let dict = countWords file_content
+    let top_ten = take 10 $ topCountedWords dict
+    putStr $ formatLines top_ten
+
+countWords :: Integral n => String -> Dict String n
+countWords = foldr addWord empty . onlyValidWords . getWords
+
+addWord :: forall n. Integral n => String -> Dict String n -> Dict String n
+addWord s dict = insert s count_s dict
+    where
+        count_s :: n
+        count_s = maybe 1 (+1) $ lookup s dict
+
+onlyValidWords :: [String] -> [String]
+onlyValidWords = filter (longerThan 2)
+    where
+        longerThan :: Integral n => n -> String -> Bool
+        longerThan 0 (_:_) = True
+        longerThan n (_:cs) = longerThan (n - 1) cs
+        longerThan _ [] = False
+
+getWords :: String -> [String]
+getWords = words . map letterOtherwiseSpace
+    where
+        letterOtherwiseSpace :: Char -> Char
+        letterOtherwiseSpace c = if isLetter c then c else ' '
+
+topCountedWords :: Integral n => Dict String n -> [(String, n)]
+topCountedWords = sortBy (flip compareByOccurenceCount) . toList
+    where
+        compareByOccurenceCount :: Integral n => (String, n) -> (String, n) -> Ordering
+        compareByOccurenceCount (_, n) (_, m) = compare n m
+
+formatLines :: Integral n => [(String, n)] -> String
+formatLines = unlines . map formatPair
+    where
+        formatPair :: Integral n => (String, n) -> String
+        formatPair (s, n) = concat [s, " ", show n]
--- haskell/dict/Splay.hs
+++ haskell/dict/Splay.hs
@@ -10,58 +10,58 @@ module Splay
 
 import Prelude hiding(lookup)
 
-data (Ord k) => Dict k v =
+data Ord k => Dict k v =
     Node (k, v) (Dict k v) (Dict k v)
   | Empty
     deriving (Show)
 
-empty :: Dict k v
+empty :: Ord k => Dict k v
 empty = Empty
 
-insert :: (Ord k) => k -> v -> Dict k v -> Dict k v
+insert :: Ord k => k -> v -> Dict k v -> Dict k v
 insert k v d = splay k $ insert' k v d
     where
-        insert' :: (Ord k) => k -> v -> Dict k v -> Dict k v
+        insert' :: Ord k => k -> v -> Dict k v -> Dict k v
         insert' k v Empty = Node (k, v) Empty Empty
         insert' k v (Node p@(k', _) l r) = case compare k k' of
             LT -> Node p (insert k v l) r
             GT -> Node p l (insert k v r)
             EQ -> Node (k, v) l r
 
-lookup :: (Ord k) => k -> Dict k v -> Maybe v
+lookup :: Ord k => k -> Dict k v -> Maybe v
 lookup k n = case splay k n of
     Empty -> Nothing
     (Node (k', v) _ _) -> if k == k' then Just v else Nothing
 
-fromList :: (Ord k) => [(k, v)] -> Dict k v
-fromList = foldl (flip $ uncurry insert) Empty
+fromList :: Ord k => [(k, v)] -> Dict k v
+fromList = foldr (uncurry insert) Empty
 
-toList :: (Ord k) => Dict k v -> [(k, v)]
+toList :: Ord k => Dict k v -> [(k, v)]
 toList Empty = []
-toList (Node p l r) = (toList l) ++ (p : toList r)
+toList (Node p l r) = concat [toList l, [p], toList r]
 
-splay :: (Ord k) => k -> Dict k v -> Dict k v
+splay :: Ord k => k -> Dict k v -> Dict k v
 splay k Empty = Empty
-splay k n@(Node (k', _) _ _) = case compare k k' of
-    EQ -> n
-    o -> case node o n of
-        Empty -> n
-        nn@(Node (k'', _) _ _) -> case compare k k'' of
-            EQ -> rot1 o n
-            oo -> case node oo nn of
-                Empty -> rot1 o n
-                (Node _ _ _) -> rot2 o oo n $ splay k
+splay k n1@(Node (k', _) _ _) = case compare k k' of
+    EQ -> n1
+    o1 -> case node o1 n1 of
+        Empty -> n1
+        n2@(Node (k'', _) _ _) -> case compare k k'' of
+            EQ -> rot1 o1 n1
+            o2 -> case node o2 n2 of
+                Empty -> rot1 o1 n1
+                (Node _ _ _) -> rot2 o1 o2 n1 $ splay k
 
-node :: (Ord k) => Ordering -> Dict k v -> Dict k v
+node :: Ord k => Ordering -> Dict k v -> Dict k v
 node LT (Node _ l _) = l
 node GT (Node _ _ r) = r
 
-rot1 :: (Ord k) => Ordering -> Dict k v -> Dict k v
+rot1 :: Ord k => Ordering -> Dict k v -> Dict k v
 rot1 LT (Node p (Node lp ll lr) r) = Node lp ll (Node p lr r)
 rot1 GT (Node p l (Node rp rl rr)) = Node rp (Node p l rl) rr
 rot1 _ _ = error "No such rotation"
 
-rot2 :: (Ord k) => Ordering -> Ordering -> Dict k v -> (Dict k v -> Dict k v) -> Dict k v
+rot2 :: Ord k => Ordering -> Ordering -> Dict k v -> (Dict k v -> Dict k v) -> Dict k v
 rot2 LT LT (Node p (Node lp ll lr) r) splay' = case splay' ll of
     (Node llp lll llr) -> Node llp lll (Node lp llr (Node p lr r))
 rot2 LT GT (Node p (Node lp ll lr) r) splay' = case splay' lr of
