a6ce1be4d965b7baf10b3e7f5d1fc3296c375115
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2011-03-07 11:55:27 +0100

    Lab03

--- haskell/Lab03.hs
+++ haskell/Lab03.hs
@@ -2,11 +2,6 @@ module Lab03 where
 import Control.Monad
 import Network.CGI.Protocol (maybeRead)
 
-instance Monad (Either a) where
-  return = Right
-  Right m >>= k = k m
-  Left e >>= _ = Left e
-
 -- Zadanie 1
 readInts :: Integral n => Read n => String -> Either String [n]
 readInts s = maybe (Left "Error") return $ foldr (liftM2 (:)) (return []) $ map maybeRead $ words s
@@ -16,9 +11,19 @@ sumInts s = either id (unwords . map show) $ (readInts s) >>= (return . (:[]) . 
 
 -- Zadanie 4
 sequence1 :: Monad m => [m a] -> m [a]
-sequence1 l = reversed >>= (return . reverse)
-  where
-    reversed = foldl (liftM2 $ flip (:)) (return []) l
+-- sequence1 = foldl (liftM2 $ flip (:)) (return [])
+sequence1 [] = return []
+sequence1 (x:xs) = do
+    x' <- x
+    xs' <- sequence1 xs
+    return (x':xs')
+
+
+mapM1 :: Monad m => (a -> m b) -> [a] -> m [b]
+mapM1 f = sequence . map f
+
+forM1 :: Monad m => [a] -> (a -> m b) -> m [b]
+forM1 = flip mapM
 
 -- Zadanie 5
 allPairs1 :: [a] -> [a] -> [[a]]
@@ -26,8 +31,8 @@ allPairs1 xs ys = [[x, y] | x <- xs, y <- ys]
 
 allPairs2 :: [a] -> [a] -> [[a]]
 allPairs2 xs ys = do
-  x <- xs
-  y <- ys
-  return [x, y]
+    x <- xs
+    y <- ys
+    return [x, y]
 
 
--- haskell/Lab03Parser.hs
+++ haskell/Lab03Parser.hs
@@ -1,40 +1,41 @@
 module Lab03Parser where
 import Control.Monad.Error
+import Data.Char
 
-instance Monad (Either a) where
-  return = Right
-  Right m >>= k = k m
-  Left e >>= _ = Left e
+--instance Monad (Either a) where
+--  return = Right
+--  Right m >>= k = k m
+--  Left e >>= _ = Left e
 
 data ParseError = Err {location::Integer, reason::String}
 instance Error ParseError where
     noMsg = Err 0 "Unknown reason"
-    strMsg s = Err ((words s) !! 2) "Parse error"
+    strMsg s = Err (read $ (words s) !! 2) "Parse error"
+
+niceMessage :: ParseError -> String
+niceMessage (Err i r) = concat ["Error at col: ", show i, "; with message: ", r]
 
 type ParseMonad = Either ParseError
 
 parseHexDigit :: Char -> Integer -> ParseMonad Integer
-parseHexDigit c i = case c of
-    '0' -> 0
-    '1' -> 1
-    '2' -> 2
-    '3' -> 3
-    '4' -> 4
-    '5' -> 5
-    '6' -> 6
-    '7' -> 7
-    '8' -> 8
-    '9' -> 9
-    'a' -> 10
-    'b' -> 11
-    'c' -> 12
-    'd' -> 13
-    'e' -> 14
-    'f' -> 15
-    _ -> error $ concat ["Character at ", show i, " wasnt matched a hex digit"]
+parseHexDigit c i =
+    if isHexDigit c
+        then return $ toInteger c_i
+        else throwError $ strMsg $ concat ["Character at ", show i, " wasnt matched a hex digit"]
+    where
+        c_i :: Int
+        c_i = if '0' <= c && c <= '9'
+            then ord c - ord '0'
+            else if 'A' <= c && c <= 'F'
+                then 10 + ord c - ord 'A'
+                else 10 + ord c - ord 'a'
 
 parseHex :: String -> ParseMonad Integer
-parseHex s = liftM (zip [0..]) $ foldrM (\el acc -> return (el:acc)) (return [])
+parseHex s = liftM listToInt $ sequence $ map (uncurry parseHexDigit) $ zip s [0..]
+    where
+        listToInt :: [Integer] -> Integer
+        listToInt l = sum $ map (uncurry (*)) $ zip logs $ reverse l
+        logs = 1:(map (*16) logs)
 
 toString :: Integer -> ParseMonad String
 toString = return . show
@@ -42,5 +43,5 @@ toString = return . show
 convert :: String -> String
 convert s = str where
     (Right str) = tryParse s `catchError` printError
-    tryParse s = do {n <- parseHex s; toString n}
-    printError e = 
+    tryParse s = do { n <- parseHex s; toString n }
+    printError e = return $ niceMessage e
