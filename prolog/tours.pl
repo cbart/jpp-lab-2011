f3c943cc18ac150bd3e9832eb4cc536c3c0bce9b
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2011-05-21 17:16:50 +0200

    Prolog 2

--- prolog/tours.pl
+++ prolog/tours.pl
@@ -1,13 +1,43 @@
-user:main(start) :-
+user:runtime_entry(start) :-
     current_prolog_flag(argv, [ToursFile]) ->
-        process(ToursFile);
+        process(ToursFile)
+    ;
 	    write('Usage: tours <tours_file.txt>\n').
 
 process(ToursFile) :-
+    read_tours(ToursFile, Tours),
+    print_tours(Tours).
+
+read_tours(ToursFile, Tours) :-
     see(ToursFile),
-    repeat,
-    get_char(C),
-    print(C),
-    C = end_of_file,
-    !,
+    get_char(Char),
+    read_tokens(Char, [], TokensList),
+    split_by(TokensList, [' ', ':', '(', ',', ')', '.', '\n'], [], [], Tours),
     seen.
+
+read_tokens(end_of_file, TokensList, TokensList).
+read_tokens(Char, Acc, TokensList) :-
+    get_char(AnotherChar),
+    read_tokens(AnotherChar, [Char | Acc], TokensList).
+
+split_by([], _, [], Splitted, Splitted).
+split_by([], _, TokenAccList, Acc, Splitted) :-
+    name(TokenAcc, TokenAccList),
+    Splitted = [TokenAcc | Acc].
+split_by([Token | Tokens], Splitters, TokenAcc, Acc, Splitted) :-
+    member(Token, Splitters) ->
+        (TokenAcc = [] ->
+            split_by(Tokens, Splitters, [], Acc, Splitted)
+        ;
+            name(NewToken, TokenAcc),
+            split_by(Tokens, Splitters, [], [NewToken | Acc], Splitted)
+        )
+    ;
+        name(Token, [TokenNum]),
+        split_by(Tokens, Splitters, [TokenNum | TokenAcc], Acc, Splitted).
+
+print_tours([]) :- !.
+print_tours([Tour|RestOfTours]) :-
+    write(Tour),
+    nl,
+    print_tours(RestOfTours).
